84956deb113f20d7c25c82535b2da91d4526119e
TulaMagar <36879268+IshkMagar@users.noreply.github.com>
2019-12-13 13:25:34 -0500

    computing club

--- jscript/imageslideshow.js
+++ jscript/imageslideshow.js
@@ -1,3 +1,28 @@
+function show(){
+    var x = document.getElementById("showw");
+    if (x.style.display === "none")
+    {
+       x.style.display = "block";
+       //x.style.class="w3-dropdown-hover"
+      // document.getElementById("Close").innerHTML = "Close";
+       x.style.width = "100%";
+    }
+    else{
+       x.style.display = "none";
+    }
+}
+function expand(number){
+    var y = event.target.firstElementChild;
+    if(y.style.display==="none")
+    {
+       y.style.display = "block";
+
+    }
+    else{
+       y.style.display = "none";
+    }
+}
+
 var images=["computerclubimage.jpg","Computing-feat.jpg", "computingimage.jpg","shutterstock.jpg"]
 automatic();
 var count =0;
